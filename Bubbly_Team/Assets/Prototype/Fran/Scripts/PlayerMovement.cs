f888db2e0c22e34cbaa70d3966a88cae7361429e
fdezbarroso <f.fdezbarroso@gmail.com>
2025-01-25 17:54:23 +0100

    Improve boost juicyness

--- Bubbly_Team/Assets/Prototype/Fran/Scripts/PlayerMovement.cs
+++ Bubbly_Team/Assets/Prototype/Fran/Scripts/PlayerMovement.cs
@@ -1,22 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float swimVelocity = 0.1f;
+    [SerializeField] private float swimVelocity = 1.0f;
     [SerializeField] private float rotateVelocity = 1.0f;
+
     [SerializeField] private float boostVelocity = 1.0f;
-    [SerializeField] private float boostDelay = 0.5f;
+    [SerializeField] private float boostDuration = 1.0f;
+    [SerializeField] private AnimationCurve boostCurve;
 
     private Vector2 _mouseWorldPosition = new Vector2();
     private Vector2 _playerToMouseDirection = new Vector2();
 
     private float _playerRotationDeg = 0.0f;
     private bool _boosting = false;
+    private float _boostSpeedCurrent = 0.0f;
+    private float _boostTimeCurrent = 0.0f;
 
     private Rigidbody2D _rb;
 
@@ -28,31 +33,48 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         _mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log(mouseWorldPosition);
 
         _playerToMouseDirection = new Vector2(_mouseWorldPosition.x - transform.position.x,
             _mouseWorldPosition.y - transform.position.y).normalized;
 
-        if (!_boosting && Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (!_boosting)
         {
-            _rb.velocity = _playerToMouseDirection * boostVelocity;
-            _boosting = true;
+            if (Mouse.current.leftButton.wasPressedThisFrame || Keyboard.current.spaceKey.wasPressedThisFrame)
+            {
+                _boostSpeedCurrent = boostVelocity * boostCurve.Evaluate(0.0f);
+                _rb.velocity = transform.right * _boostSpeedCurrent;
 
-            StartCoroutine(ExampleCoroutine());
+                _boostTimeCurrent = 0.0f;
+                _boosting = true;
+            }
+            else
+            {
+                _rb.velocity = transform.right * swimVelocity;
+            }
         }
-    }
+        else
+        {
+            if (_boostTimeCurrent >= boostDuration)
+            {
+                _boosting = false;
+            }
+            else
+            {
+                _boostSpeedCurrent =
+                    Mathf.Clamp(boostVelocity * boostCurve.Evaluate(_boostTimeCurrent / boostDuration), swimVelocity,
+                        boostVelocity);
+                _rb.velocity = transform.right * _boostSpeedCurrent;
 
-    IEnumerator ExampleCoroutine()
-    {
-        yield return new WaitForSeconds(boostDelay);
-        _boosting = false;
+                _boostTimeCurrent += Time.deltaTime;
+            }
+        }
     }
 
     private void FixedUpdate()
     {
         _playerRotationDeg = Mathf.Atan2(_playerToMouseDirection.y, _playerToMouseDirection.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.RotateTowards(transform.rotation,
-            Quaternion.Euler(0f, 0f, _playerRotationDeg - 90.0f), rotateVelocity * Time.deltaTime);
+            Quaternion.Euler(0f, 0f, _playerRotationDeg), rotateVelocity * Time.deltaTime);
     }
 
     //Jose
