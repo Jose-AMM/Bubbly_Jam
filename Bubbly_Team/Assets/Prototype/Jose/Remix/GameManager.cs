e33b048b6db0db67822e076a9bc42e35450975fd
fdezbarroso <f.fdezbarroso@gmail.com>
2025-01-26 15:11:21 +0100

    Fix gitignore and cursor

--- Bubbly_Team/Assets/Prototype/Jose/Remix/GameManager.cs
+++ Bubbly_Team/Assets/Prototype/Jose/Remix/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private BlackPanelLogic BlackPanel;
     [SerializeField] private List<GameObject> ShopPrefabs;
     private GameObject InstantiatedShop;
-    public static GameManager Instance  { get; private set; }
+    public static GameManager Instance { get; private set; }
 
     void Awake()
     {
@@ -54,7 +54,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         float SoundVolume = 1.0f;
-        
+
         if (Input.GetKey(KeyCode.Alpha1))
         {
             SoundManager.Instance.PlaySound("AHOGANDOSE", SoundVolume);
@@ -64,30 +64,37 @@ public class GameManager : MonoBehaviour
         {
             SoundManager.Instance.PlaySound("DASH", SoundVolume);
         }
+
         if (Input.GetKey(KeyCode.Alpha3))
         {
             SoundManager.Instance.PlaySound("GOLPE", 1.3f);
         }
+
         if (Input.GetKey(KeyCode.Alpha4))
         {
             SoundManager.Instance.PlaySound("PECES", 1.4f);
         }
+
         if (Input.GetKey(KeyCode.Alpha5))
         {
             SoundManager.Instance.PlaySound("RESPIRACION", SoundVolume);
-        }        
+        }
+
         if (Input.GetKey(KeyCode.Alpha6))
         {
             SoundManager.Instance.PlaySound("SIRENA", 0.7f);
         }
     }
-    public Vector2 GetPlayerPos(){
+
+    public Vector2 GetPlayerPos()
+    {
         return new Vector2(Player.transform.position.x, Player.transform.position.y);
     }
-    public void DisablePlayer(){
+
+    public void DisablePlayer()
+    {
         Player.GetComponent<PlayerMovement>().Stop();
         Player.GetComponent<JellyfishFloatSimple>().enabled = true;
-        Cursor.visible = true;
     }
 
     public void KillJellyfish()
@@ -95,7 +102,8 @@ public class GameManager : MonoBehaviour
         Player.GetComponent<JellyfishFloatSimple>().enabled = false;
     }
 
-    public void GetOxygen(){
+    public void GetOxygen()
+    {
         Player.transform.Find("Canvas/OxigenBar").GetComponent<OxygenBar>().AddOxygen(100.0f);
         SoundManager.Instance.PlaySound("Gasp", 0.2f);
     }
@@ -112,18 +120,23 @@ public class GameManager : MonoBehaviour
         StartCoroutine(UnloadShop());
     }
 
-    IEnumerator LoadShop(String PrefabName){
+    IEnumerator LoadShop(String PrefabName)
+    {
+        Cursor.visible = true;
         BlackPanel.gameObject.SetActive(true);
         yield return new WaitForSeconds(0);
         BlackPanel.StartFadeIn();
         DisablePlayer();
         yield return new WaitForSeconds(BlackPanel.fadeDuration);
         Debug.Log("Load Prefab: " + PrefabName);
-        foreach (GameObject ShopPrefab in ShopPrefabs){
-            if (ShopPrefab.name == PrefabName){
+        foreach (GameObject ShopPrefab in ShopPrefabs)
+        {
+            if (ShopPrefab.name == PrefabName)
+            {
                 InstantiatedShop = Instantiate(ShopPrefab, Vector3.zero, Quaternion.identity);
-            }  
+            }
         }
+
         yield return new WaitForSeconds(0.2f);
         BlackPanel.StartFadeOut();
         yield return new WaitForSeconds(BlackPanel.fadeDuration);
@@ -190,6 +203,7 @@ public class GameManager : MonoBehaviour
                         }
                     }
                 }
+
                 break;
             case "SHOP4":
                 allDialogues = GameObject.FindObjectsOfType<DialogueTrigger>();
@@ -200,12 +214,11 @@ public class GameManager : MonoBehaviour
                         t.TriggerDialogue();
                     }
                 }
+
                 break;
             default:
                 break;
         }
-
-
     }
 
     public IEnumerator UnloadShop()
@@ -226,7 +239,6 @@ public class GameManager : MonoBehaviour
         Player.GetComponent<PlayerMovement>().AsNew();
         Player.GetComponent<JellyfishFloatSimple>().enabled = false;
         //Player.SetActive(true);
-        Cursor.visible = false;
     }
 
     public void BlackScreenFadeOut()
@@ -312,7 +324,7 @@ public class GameManager : MonoBehaviour
 
     public void NextCheckpoint()
     {
-        if (currentCheckpoint+1 >= maxCheckpointsSize)
+        if (currentCheckpoint + 1 >= maxCheckpointsSize)
         {
             currentCheckpoint = 0;
         }
