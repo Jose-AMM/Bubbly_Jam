e61bb0cb91c480d647e4915b38545d47a3ac374c
fdezbarroso <f.fdezbarroso@gmail.com>
2025-01-26 14:24:03 +0100

    Fix collider function tileset 4

--- Bubbly_Team/Assets/Prototype/Fran/Scripts/TPTrigger.cs
+++ Bubbly_Team/Assets/Prototype/Fran/Scripts/TPTrigger.cs
@@ -7,7 +7,7 @@ public class TPTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject respawn;
 
-    void OnTriggerEnter2D()
+    void OnTriggerEnter2D(Collider2D other)
     {
         GameManager.Instance.TPPlayerToPosition(respawn.transform.position);
     }
